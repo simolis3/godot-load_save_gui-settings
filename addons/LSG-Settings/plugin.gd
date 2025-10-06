33cf3837f386907ed00967c11e31d4f4d72fff88
simolis3 <simolin94@gmail.com>
2025-10-06 10:53:23 +0300

    fix script paths

--- addons/LSG-Settings/plugin.gd
+++ addons/LSG-Settings/plugin.gd
@@ -3,13 +3,15 @@ extends EditorPlugin
 
 const SETTINGS_PANEL_TYPE_NAME : String = "SettingsPanel"
 const SETTINGS_MANAGER_TYPE_NAME : String = "SettingsManager"
-const SETTINGS_PANEL_SCRIPT = preload("res://addons/LSG-Settings/settings_panel.gd")
-const SETTINGS_MANAGER_SCRIPT = preload("res://addons/LSG-Settings/settings_manager.gd")
 const SETTINGS_PANEL_ICON = preload("res://addons/LSG-Settings/settings_panel_icon.png")
+const SETTINGS_MANAGER_SCRIPT_PATH = "res://addons/LSG-Settings/settings_manager.gd"
+
+var SETTINGS_PANEL_SCRIPT = preload("res://addons/LSG-Settings/settings_panel.gd")
+
 
 func _enter_tree() -> void:
 	add_custom_type(SETTINGS_PANEL_TYPE_NAME, "VBoxContainer", SETTINGS_PANEL_SCRIPT, SETTINGS_PANEL_ICON)
-	add_autoload_singleton(SETTINGS_MANAGER_TYPE_NAME, SETTINGS_MANAGER_SCRIPT)
+	add_autoload_singleton(SETTINGS_MANAGER_TYPE_NAME, SETTINGS_MANAGER_SCRIPT_PATH)
 
 
 func _exit_tree() -> void:
